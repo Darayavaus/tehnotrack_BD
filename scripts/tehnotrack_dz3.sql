6dcfe2f218a217f685687ddf09075084d79e86db
Darayavaus <bor.astat@gmail.com>
2018-10-10 16:38:38 +0300

    dz3-added reslts

--- scripts/tehnotrack_dz3.sql
+++ scripts/tehnotrack_dz3.sql
@@ -6,13 +6,30 @@ select
     group by u.user_id
     order by sum(p.payment_sum) desc
     limit 3;
+    
+-- result : 
+-- +----------+
+-- | login    |
+-- +----------+
+-- | login_82 |
+-- | login_38 |
+-- | login_85 |
+-- +----------+
+
  
 select sum(subq.sessions)/count(*) from
-	(select 
-		u.login as login, count(s.session_id) as sessions
+	(select u.login as login, count(s.session_id) as sessions
 		from users as u left join sessions as s on u.user_id = s.user_id
 		group by u.login) as subq;
         
 -- или: 
 
 select count(s.session_id)/(select count(u.user_id) from users as u) from sessions as s;
+
+
+-- result: 
+-- +-----------------------------+
+-- | sum(subq.sessions)/count(*) |
+-- +-----------------------------+
+-- |                     35.9100 |
+-- +-----------------------------+
